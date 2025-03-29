36d4917b2e9a5d46cff6f5137be4a07ea4e7f8ab
Adonis95 <Sober4202020@outlook.com>
2025-03-29 15:51:13 -0600

    practice converting sql data into an Excel file

--- practice.py
+++ practice.py
@@ -19,16 +19,43 @@ password = os.getenv("DB_PASSWORD")
 
 engine = create_engine(f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server")
 
-try:
-    with engine.connect() as conn:
-        print("Successfull")
-except Exception as e:
-    print(f"Error {e}")
+
+def connect_and_export():
+
+    #Connects to the databse, retrieves data then saves it to an Excel file
+
+    try:
+        with engine.connect() as conn:
+            
+            query = "SELECT * FROM Homes"
+
+            df = pd.read_sql(query, conn)
+
+            excel_file = "homes_info.xlsx"
+
+            df.to_excel(excel_file, index=False)
+
+            print(f"The data has been written to {excel_file}")
+    except Exception as e:
+        print("Error occured, {e}")
 
 
-def select_all():
-    query = "SELECT * FROM Homes"
-    df = pd.read_sql(query, engine)
-    print(df)
+connect_and_export()
+
+
+
+# try:
+#     with engine.connect() as conn:
+#         print("Successfull")
+
+        
+# except Exception as e:
+#     print(f"Error {e}")
+
+
+# def select_all():
+#     query = "SELECT * FROM Homes"
+#     df = pd.read_sql(query, engine)
+#     print(df)
 
 
